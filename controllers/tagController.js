945a3e0597cff1e346db036cbc8f9c19f29224a9
DamianoCasolari <damianocasolari@gmail.com>
2023-11-27 21:50:53 +0100

    add check schema to validate tags

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,20 +1,30 @@
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
-
+const { validationResult, matchedData, } = require("express-validator")
+const customError = require("../utilities/customErrors")
 
 async function store(req, res, next) {
 
+    const validation = validationResult(req)
+
+    if (!validation.isEmpty()) {
+        // const errorMessages = validation.array().map(error => error.msg);
+        // next( new customError( errorMessages.join(', '), 400))
+        return res.status(422).json(validation.array());
+    }
+
+    const reqBody = matchedData(req)
     try {
-    const data = await prisma.tag.create({
+        const data = await prisma.tag.create({
             data: {
-                name: req.body.name,
-                description: req.body.description
+                name: reqBody.name,
+                description: reqBody.description
             }
 
         })
 
         return res.json(data)
-    }catch(error){
+    } catch (error) {
         console.error("Errore durante l'elaborazione della richiesta:", error);
         next(new Error("Errore interno del server"))
     }
--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
-
 const tagController = require("../controllers/tagController")
+const tagsCreateValidation = require("../validation/tagsCreateValidation")
+
+const {checkSchema} = require("express-validator")
 
 
-router.post("/",tagController.store )
 
+router.post("/",checkSchema(tagsCreateValidation), tagController.store )
 module.exports = router
