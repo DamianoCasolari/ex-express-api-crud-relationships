21878bf773c9203cfe37c4070d53d6bacfe6b840
DamianoCasolari <damianocasolari@gmail.com>
2023-11-27 18:41:14 +0100

    first elementar validation on category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,10 +1,17 @@
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
+const { validationResult } = require("express-validator")
+const customError = require("../utilities/customErrors")
 
 async function store(req, res, next) {
 
+    const validation = validationResult(req)
+
+    if (!validation.isEmpty()) {
+        next(new customError("Dati non conformi", 400))
+    }
     try {
-    const data = await prisma.category.create({
+        const data = await prisma.category.create({
             data: {
                 name: req.body.name,
                 description: req.body.description
@@ -13,7 +20,7 @@ async function store(req, res, next) {
         })
 
         return res.json(data)
-    }catch(error){
+    } catch (error) {
         console.error("Errore durante l'elaborazione della richiesta:", error);
         next(new Error("Errore interno del server"))
     }
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,7 +4,10 @@ const categoryController = require("../controllers/categoryController")
 const { body} = require("express-validator")
 
 
-router.post("/", categoryController.store)
+router.post("/",
+body("name").isString().notEmpty().isLength({min:3, max: 30}), 
+body("description").isString().notEmpty(), 
+categoryController.store)
 
 
 module.exports = router
